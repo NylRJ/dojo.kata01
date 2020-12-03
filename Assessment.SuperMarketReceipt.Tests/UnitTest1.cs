cb70573ec7e94dbdf51bd5e5baafa45b13669cac
Moises R. Souza <loveloungeoficial@gmail.com>
2020-12-02 23:50:22 -0300

    Create Project

---
+++ Assessment.SuperMarketReceipt.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Assessment.SuperMarketReceipt.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Assessment.SuperMarketReceipt/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Assessment.SuperMarketReceipt
+{
+    public class Class1
+    {
+    }
+}
